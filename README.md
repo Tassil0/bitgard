c2436b6f9d8e62a5752ad4abe088540ae6cc578f
Filip Sikora <contact@fsikora.com>
2022-11-14 02:03:39 +0100

    :wheelchair: fuck this make it a todo app

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # bitgard
 GTK Bitwarden desktop client written in Rust.
+
+packages
+```
+
+```
+
+install schema
+
+```
+mkdir -p $HOME/.local/share/glib-2.0/schemas
+cp com.tassilobalbo.bitgard.gschema.xml $HOME/.local/share/glib-2.0/schemas/
+glib-compile-schemas $HOME/.local/share/glib-2.0/schemas/
+```
--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,37 @@
+mod collection_object;
+mod task_object;
+mod utils;
+mod window;
+
 use adw::prelude::*;
+use gtk::gio;
+use window::Window;
 
-use adw::{ActionRow, Application, ApplicationWindow, gio, HeaderBar, Window};
-use gtk::{Box, ListBox, Orientation, SelectionMode};
-
-const APP_ID: &str = "com.tassilobalbo.bitgard";
+static APP_ID: &str = "com.tassilobalbo.bitgard";
 
 fn main() {
-    // Register and include resources
     gio::resources_register_include!("bitgard.gresource")
         .expect("Failed to register resources.");
 
-    let app = Application::builder()
-        .application_id(APP_ID)
-        .build();
+    // Create a new application
+    let app = adw::Application::builder().application_id(APP_ID).build();
 
+    // Connect to signals
+    app.connect_startup(setup_shortcuts);
     app.connect_activate(build_ui);
 
-    /*app.connect_activate(|app| {
-        let mut rows: Vec<ActionRow> = Vec::new();
-
-        for i in 0..10 {
-            rows.push(ActionRow::builder()
-                            .activatable(true)
-                            .title("Click me")
-                            .build());
-        }
-
-        // ActionRows are only available in Adwaita
-        let row = ActionRow::builder()
-            .activatable(true)
-            .title("Click me")
-            .build();
-        row.connect_activated(|_| {
-            eprintln!("Clicked!");
-        });
-
-        let list = ListBox::builder()
-            .margin_top(32)
-            .margin_end(32)
-            .margin_bottom(32)
-            .margin_start(32)
-            .selection_mode(SelectionMode::None)
-            // makes the list look nicer
-            .css_classes(vec![String::from("boxed-list")])
-            .build();
-
-        for i in 0..10 {
-            list.append(&rows[i]);
-        }
-
-        let header = HeaderBar::builder()
-            .title("ahojki")
-            .show_close_button(true)
-            .build();
-
-        // Combine the content in a box
-        let content = Box::new(Orientation::Vertical, 0);
-        // Adwaitas' ApplicationWindow does not include a HeaderBar
-        content.append(&HeaderBar::new());
-        content.append(&list);
-
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("First App")
-            .default_width(350)
-            // add content to window
-            .content(&content)
-            .build();
-        window.show();
-    });*/
-
+    // Run the application
     app.run();
 }
 
-fn build_ui(app: &Application) {
-    let window = Window::new();
-    //window.show();
-    window.present();
+fn setup_shortcuts(app: &adw::Application) {
+    app.set_accels_for_action("win.filter('All')", &["<Ctrl>a"]);
+    app.set_accels_for_action("win.filter('Open')", &["<Ctrl>o"]);
+    app.set_accels_for_action("win.filter('Done')", &["<Ctrl>d"]);
+}
+
+fn build_ui(app: &adw::Application) {
+    // Create a new custom window and show it
+    let window = Window::new(app);
+    window.show();
 }
